9e5764c2fff92dc9022216aa5ee1929992a23423
ruslantuchkov <nidjel87@gmail.com>
2018-03-01 12:02:51 +0500

    complete authentication

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const coockieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
@@ -11,7 +11,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-  coockieSession({
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
--- services/passport.js
+++ services/passport.js
@@ -6,12 +6,12 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-  //устанавливает coockies с user id
+  //устанавливает cookies с user id
   done(null, user.id); //user из done в User.findOne ниже, user.id - id созданный mongo
 });
 
 passport.deserializeUser((id, done) => {
-  //извлекает user id из coockies
+  //извлекает user id из cookies
   User.findById(id).then(user => {
     done(null, user);
   });
