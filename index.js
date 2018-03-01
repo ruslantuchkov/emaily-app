0054ca07507205398724453bf8e5bc2e393b622c
ruslantuchkov <nidjel87@gmail.com>
2018-03-01 09:18:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('server is running on port ' + PORT));
