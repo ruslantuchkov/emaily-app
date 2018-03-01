d28fa8eaf5200028fa226e6bff5651963196ad2d
ruslantuchkov <nidjel87@gmail.com>
2018-03-01 11:15:34 +0500

    added User model and setting coockies with user id

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //user из done в User.findOne ниже, user.id - id созданный mongo
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +16,16 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
