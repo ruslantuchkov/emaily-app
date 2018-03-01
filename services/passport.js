98281c04315320172a34dfd0eed3847d86f22d28
ruslantuchkov <nidjel87@gmail.com>
2018-03-01 11:44:38 +0500

    enabling cokkies

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,15 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
+  //устанавливает coockies с user id
   done(null, user.id); //user из done в User.findOne ниже, user.id - id созданный mongo
+});
+
+passport.deserializeUser((id, done) => {
+  //извлекает user id из coockies
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
